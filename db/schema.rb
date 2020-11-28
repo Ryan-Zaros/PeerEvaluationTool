6dca92b42737f3907a2f4e3c1ac6d2f7d85330ab
Ryan <shaffer.555@buckeyemail.osu.edu>
2020-11-28 17:02:02 -0500

    navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_27_231043) do
+ActiveRecord::Schema.define(version: 2020_11_27_225011) do
 
   create_table "assignments", force: :cascade do |t|
     t.text "name"
