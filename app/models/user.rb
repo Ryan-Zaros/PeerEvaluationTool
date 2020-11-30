f12eb93633cc5e32aadc9a533537ab532483733d
Omer Alrefaai <alrefaai.1@buckeyemail.osu.edu>
2020-11-30 05:32:57 -0500

    Added  authentication features

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  belongs_to :role
-  has_many :evaluations
-  has_many :evaluation_scores
-  has_many :groups, through: :user_groups
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
