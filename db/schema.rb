14e1dd225be7b991b4f736a633c95eba62b0bd55
Ryan <shaffer.555@buckeyemail.osu.edu>
2020-11-30 14:01:18 -0500

    migrates, navbar changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_30_081930) do
+ActiveRecord::Schema.define(version: 2020_11_30_081853) do
 
   create_table "assignments", force: :cascade do |t|
     t.text "name"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2020_11_30_081930) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -71,7 +72,6 @@ ActiveRecord::Schema.define(version: 2020_11_30_081930) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin", default: false
-    t.text "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
